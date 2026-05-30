0b19bab363440991692483fce6a0d593ba4d6b08
Darrell Jones <2424djones@gmail.com>
2026-05-30 10:22:20 -0400

    update config

--- hooks/config.tsx
+++ hooks/config.tsx
@@ -1,8 +1,8 @@
 
-export const API_KEY = 'da2-3mhmnkuv7jfjxetdywiwm4fmlm';
+export const API_KEY = 'da2-2rckfio2czgrdbnqv27f2jnr5a';
 export const URL = 'https://zx427q6qivddlgophnz4kn652i.appsync-api.us-east-1.amazonaws.com/graphql';
 
 export const headers = {
     'content-type': 'application/json',
-    'X-API-KEY': 'da2-3mhmnkuv7jfjxetdywiwm4fmlm',
+    'X-API-KEY': 'da2-2rckfio2czgrdbnqv27f2jnr5a',
   };
